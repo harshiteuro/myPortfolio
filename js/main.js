c625227624abf65e959cac4db37a4baa1e730d00
harshit <harshiteuro@gmail.com>
2021-06-11 02:27:47 +0530

    files updated

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ $(document).ready(function () {
     let $btns = $('.project-area .button-group button');
 
 
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbz1bnFhF4_7-dmYGBIlenJ7A3TLb-jDvIvJoeCYYqV_H6VylsfuXV6djaSAHOr8D7ts/exec'
+            const form = document.forms['google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Thanks for Contacting us..!"))
+                .catch(error => console.error('Error!', error.message))
+            })
+
     $btns.click(function (e) {
 
         $('.project-area .button-group button').removeClass('active');
